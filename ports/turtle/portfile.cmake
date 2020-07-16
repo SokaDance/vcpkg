384bfa75da75b9be60c1149ddbaa8c7677321310
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2020-07-16 11:12:55 +0200

    [turtle] update to version 1.3.2

--- ports/turtle/portfile.cmake
+++ ports/turtle/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mat007/turtle
-    REF v1.3.1
-    SHA512 2a79a23ca7da669a11012dea98216aad44d2bed1be4dd4b75e5fbb07a7ba61f87a117284157bd117698544c31eefe06df8621727acd287ab6530a0138f524434
+    REF v1.3.2
+    SHA512 be6f6097d522fc2b96157692dbb5dc7a1ab47b8955956fe9955d4d1477958ed444c8e1a53fe616905669d0ec450eca7eddef6038ac137763bc02bbcbe2ffcf9d
     HEAD_REF master
 )
 
